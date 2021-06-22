0166355910892c65744ef2f139476547fb4fc4b1
Habibur Rahman Anik <habib264521@gmail.com>
2021-06-22 23:20:34 +0600

    fsd

--- mid_labTask/resources/views/admin/editUser.blade.php
+++ mid_labTask/resources/views/admin/editUser.blade.php
@@ -21,10 +21,10 @@
                 <ul>
                     <li><a href="{{route('admin.index')}}">Dashboard</a></li>
                     <li><a href="{{route('admin.addIndex')}}">Add User</a></li>
-                    <li><a href="userList.php">View user list</a></li>
-                    <li><a href="{{ route('product.addProductView') }}">Add Product</a></li>
+                    {{-- <li><a href="userList.php">View user list</a></li> --}}
+                    {{-- <li><a href="{{ route('product.addProductView') }}">Add Product</a></li>
 				    <li><a href="{{ route('product.upcomingProduct') }}">existing Product</a></li>
-				    <li><a href="{{ route('product.existingProduct') }}">Upcoming Product</a></li>
+				    <li><a href="{{ route('product.existingProduct') }}">Upcoming Product</a></li> --}}
                 </ul>
             </td>
             <td>
